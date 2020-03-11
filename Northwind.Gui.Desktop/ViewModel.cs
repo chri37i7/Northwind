2109fdac624731be5b437bec1bbac879922782ec
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-03-11 11:13:54 +0100

    ViewModel :D!

---
+++ Northwind.Gui.Desktop/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Northwind.DataAccess;
+using Northwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Northwind.Gui.Desktop
+{
+    public class ViewModel
+    {
+        // Fields
+        private readonly Repository repository;
+
+        // Constructor
+        public ViewModel()
+        {
+            // Initialize repository
+            repository = new Repository();
+
+            // Get all orders from the database
+            List<Order> orders = repository.GetAllOrders();
+
+            // Initialize ObServableCollection
+            Orders = new ObservableCollection<Order>(orders);
+        }
+
+        // Ban Properties
+        public ObservableCollection<Order> Orders { get; set; }
+        public Order SelectedOrder { get; set; }
+    }
+}
