8d038b7f7720f8d94627cf383d240f8bd10c5519
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-08-27 14:49:20 +0200

    Stuff

--- Northwind.Services/WebService.cs
+++ Northwind.Services/WebService.cs
@@ -1,8 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
